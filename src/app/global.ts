70d0ef127e7834e11ec1032a9c9b6c7894f4282b
Troy Harris <troyharris@users.noreply.github.com>
2017-08-06 12:57:42 -0700

    Updated version number. Fixed server address. Final commit for v0.2.0

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,5 +1,6 @@
 export const Globals = Object.freeze({
   //BASE_API_URL: 'http://10.127.0.202',
-  BASE_API_URL: 'http://127.0.0.1:8000',
-  TANDEM_VERSION: 'v0.1.2'
+  //BASE_API_URL: 'http://127.0.0.1:8000',
+  BASE_API_URL: 'http://10.127.0.202:8001',
+  TANDEM_VERSION: 'v0.2.0'
 });
